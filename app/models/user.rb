968edac025ca3f8c428bc53c7360ecbf7e614575
JusticeRises <thedoctor907@verizon.net>
2013-09-18 05:00:25 +0000

    tests all the more!!! :)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   #attr_accessor :name, :password, :password_confirmation
   #before_save { self.password = self.password_confirmation }
-  attr_reader :username, :password, :password_confirmation
-  attr_writer :username, :password, :password_confirmation
+  attr_reader :username, :password, :confirmation
+  attr_writer :username, :password, :confirmation
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 GameContestServer::Application.routes.draw do
-  # get "users#new"
+  get "users/new"
   resources :users
   get 'signup', to:'users#new', as: 'signup'
   # The priority is based upon order of creation: first created -> highest priority.
