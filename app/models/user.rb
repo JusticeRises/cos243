ccb680e27e219e56f180851ecc2bcd56abc87c47
JusticeRises <thedoctor907@verizon.net>
2013-09-18 04:55:36 +0000

    tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessor :name, :password, :password_confirmation
-  before_save { self.password = self.password_confirmation }
+  #attr_accessor :name, :password, :password_confirmation
+  #before_save { self.password = self.password_confirmation }
+  attr_reader :username, :password, :password_confirmation
+  attr_writer :username, :password, :password_confirmation
 end
