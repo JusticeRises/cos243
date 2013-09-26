82d14b2c59d9aac8d140fb8236225206d9538d81
JusticeRises <thedoctor907@verizon.net>
2013-09-26 04:47:56 +0000

    It's all cool in secondary school.  Except for the big beast of course.  That error is still ever present.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,9 @@ class User < ActiveRecord::Base
   validates :username, presence: true 
   #validates :username, presence: true 
   
+  validates_length_of :username, :maximum => 25
+  validates_uniqueness_of :username
+  
   validates :email, presence: true
   validates_format_of :email, :with =>  /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   
--- spec/requests/users_pages_spec.rb
+++ spec/requests/users_pages_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 describe "UsersPages" do
   describe "Sign Up" do
+	it "allows the user to fill in name and password" do
+      visit signup_path
+      fill_in 'Username', with: 'user@example.com'
+      fill_in 'Password', with: 'password'
+      fill_in 'Confirmation', with: 'password'
+      click_button 'Create new account'
     let(:submit) { 'Create new account' }
 
     before { visit signup_path }
@@ -29,4 +35,5 @@ describe "UsersPages" do
       end
     end
   end
+end
 end
