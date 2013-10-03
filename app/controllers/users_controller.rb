87dbcf09030cbe6bd4cbe7537cfee62880800515
JusticeRises <thedoctor907@verizon.net>
2013-10-03 01:05:17 +0000

    Ahh only 5 more tests. Freedom is beckoning yo :)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,20 @@
 class UsersController < ApplicationController
-    
-	def new
-        @user = User.new
+   
+  def index
+    @users =User.all
   end
   
-  def show
+  def edit
     @user = User.find(params[:id])
-    @microposts = @user.microposts.paginate(page: params[:page])
   end
+  
+
+  
+	def new
+    @user = User.new
+  end
+  
+  
   
   def create
     #@user = User.new(:username => params[:username],:password => params[:password])
@@ -17,12 +24,43 @@ class UsersController < ApplicationController
     #@user = User.new(params[:user])
     #@user.save
     
-    permitted_params = params.require(:user).permit(:username, :password, :password_confirmation,:email)
-		@user = User.new(permitted_params)
+    @user = User.new(acceptable_params)
 		if @user.save then
 			redirect_to @user #type of response that the server can give to the browser.
 		else
 			render 'new'
 		end
+      
   end
+    
+  def show
+    @user = User.find(params[:id])
+    ##@microposts = @user.microposts.paginate(page: params[:page])
+  end
+    
+    
+    def update
+    @user = User.find(params[:id])
+    if @user.update(acceptable_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end  
+    
+    
+  private
+    
+    def acceptable_params
+      acceptable_params = params.require(:user).permit(:username, 
+                                                       :password, 
+                                                       :password_confirmation,
+                                                       :email)
+    end
 end
